ed9b03e0b9a0602ae221b67306612fd38771bcd7
Soroush Kami <soroushkami10@gmail.com>
2024-10-23 15:01:23 -0400

    Convert objects to JSON with dumps

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ with open('data.json', 'r') as file:
     data2 = json.load(file)
 
 print(data2)
+
+data3 = {
+    "name": "Joe",
+    "age": 25,
+    "courses": ["math", "science"]
+}
+json_string = json.dumps(data3, indent=4)
+
+print(json_string)
