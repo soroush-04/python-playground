101c024dd5b33e4f5ac711cb4a901b81ec81e49d
Soroush Kami <soroushkami10@gmail.com>
2024-10-23 16:49:21 -0400

    iterate nested JSON

--- main.py
+++ main.py
@@ -42,3 +42,37 @@ students_file['students'][0]['courses']['core'].append("Computer Science")
 
 with open('students.json', 'w') as file:
     json.dump(students_file, file, indent=2)
+    
+####
+with open('students.json', 'r') as file:
+    data = json.load(file)
+
+student_name = 'Bob'
+bob_electives = None
+
+for student in data['students']:
+    if student['name'] == student_name:
+        bob_electives = student['courses']['electives']
+        break
+    
+if bob_electives:
+    print("2. Bob's Elective Courses:", bob_electives)
+else:
+    print(f"{student_name} not found.")
+    
+#### iterate users.json
+selected_users = []
+
+with open('users.json', 'r') as file:
+    data = json.load(file)
+
+for user, data in data.items():
+    if data['age'] == 20:
+        for course in data['courses']:
+            if course['name'] == 'Math':
+                selected_users.append(data)
+                break
+
+print("Users with age 20 and course 'Math':")
+for user in selected_users:
+    print(user['name'])
